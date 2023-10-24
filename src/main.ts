2086049ff20d795824aaa16c124e85d8c9581fe6
Steve Mao <maochenyan@gmail.com>
2023-10-25 10:45:26 +1100

    improve output (#9)

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ export async function run(): Promise<void> {
 
     core.debug('Getting the extra-deps')
     const extraDeps = await getExtraDeps(doc)
-    core.debug(`extra-deps: ${extraDeps}`)
+    core.debug(`extra-deps: ${JSON.stringify(extraDeps)}`)
 
     core.debug('Getting the latest versions of the extra-deps')
     const updatedExtraDeps = await Promise.all(
@@ -70,6 +70,8 @@ export async function run(): Promise<void> {
     // Set outputs for other workflow steps to use
     core.setOutput('previous-resolver', previousResolver)
     core.setOutput('new-resolver', newResolver)
+    core.setOutput('previous-extra-deps', extraDeps)
+    core.setOutput('new-extra-deps', updatedExtraDeps)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) {
