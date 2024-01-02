19141b71dde44593fd6094a8827794a44c60c657
Steve Mao <maochenyan@gmail.com>
2024-01-02 13:19:32 +1100

    fix test

--- __tests__/yaml.test.ts
+++ __tests__/yaml.test.ts
@@ -14,19 +14,19 @@ import { expect } from '@jest/globals'
 
 describe('yaml.ts', () => {
   it('should get stack.yaml', async () => {
-    const doc = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
+    const { doc } = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
 
     expect(doc.get('resolver')).toBe('lts-21.16')
   })
 
   it('should get the resolver from stack.yaml', async () => {
-    const doc = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
+    const { doc } = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
 
     expect(getResolver(doc)).toBe('lts-21.16')
   })
 
   it('should update the resolver', async () => {
-    const doc = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
+    const { doc } = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
 
     const updated = updateResolver(doc, 'lts-21.17')
 
@@ -34,7 +34,7 @@ describe('yaml.ts', () => {
   })
 
   it('should get extra deps package info', async () => {
-    const doc = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
+    const { doc } = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
 
     const extraDeps = await getExtraDeps(doc)
 
@@ -42,7 +42,7 @@ describe('yaml.ts', () => {
   })
 
   it('should set extra deps', async () => {
-    const doc = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
+    const { doc } = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
 
     const extraDeps = [
       { name: 'polysemy-zoo', version: '2' },
@@ -63,13 +63,13 @@ describe('yaml.ts', () => {
   })
 
   it('should save stack.yaml', async () => {
-    const doc = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
+    const { doc } = await getStackYaml(`${__dirname}/fixtures/stack.yaml`)
 
     const result = `${__dirname}/results/stack.yaml`
 
     await saveStackYaml(doc, result)
 
-    const updated = await getStackYaml(result)
+    const { doc: updated } = await getStackYaml(result)
 
     expect(updated.get('resolver')).toBe('lts-21.16')
   })
